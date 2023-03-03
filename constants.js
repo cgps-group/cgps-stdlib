35a00824187aafa10c8faf10b77bf1c7c69c6d1a
Khalil <khalil.abudahab@cgps.group>
2023-03-03 21:57:37 +0000

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,7 @@
+module.exports.EmptyArray = Object.freeze([]);
+
+module.exports.EmptyObject = Object.freeze({});
+
+module.exports.EmptyString = "";
+
+module.exports.EmptySet = Object.freeze(new Set());
