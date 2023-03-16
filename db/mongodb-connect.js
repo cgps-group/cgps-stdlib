1ea71eaec7b9b5835a61074fa1d9a5db9877fc42
Khalil <khalil.abudahab@cgps.group>
2023-03-16 12:39:38 +0000

    Update middleware.js

---
+++ db/mongodb-connect.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const serverRuntimeConfig = require("../config/server-runtime-config.js");
+
+const MONGODB_URL = process.env.MONGODB_URL || serverRuntimeConfig.mongodb.url;
+
+if (!MONGODB_URL) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  // eslint-disable-next-line no-multi-assign
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+module.exports = dbConnect;
