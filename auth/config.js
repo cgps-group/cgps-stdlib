a110152d41fa52205852898dda1a9e55b9bd9934
Khalil <khalil.abudahab@cgps.group>
2023-04-02 12:56:25 +0100

    Create config.js

---
+++ auth/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  google: {
+    clientId: process.env.AUTH_GOOGLE_CLIENTID,
+    clientSecret: process.env.AUTH_GOOGLE_CLIENTSECRET,
+  },
+  secret: process.env.AUTH_SECRET,
+};
