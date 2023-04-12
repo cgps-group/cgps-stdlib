b2ae143d7305dcba8c6c9d006a5d88212bc545c1
Khalil <khalil.abudahab@cgps.group>
2023-04-12 23:13:16 +0100

    Create index.js

---
+++ object-storage/index.js
@@ -0,0 +1,91 @@
+const stream = require("node:stream");
+const util = require("node:util");
+const zlib = require("node:zlib");
+
+const { S3Client, GetObjectCommand, HeadObjectCommand } = require("@aws-sdk/client-s3");
+const { Upload } = require("@aws-sdk/lib-storage");
+
+const config = require("../config/server-runtime-config.js");
+
+const pipeline = util.promisify(stream.pipeline);
+
+const client = new S3Client({
+  region: config.storageRegion,
+  credentials: {
+    accessKeyId: config.storageKey,
+    secretAccessKey: config.storageSecret,
+  },
+  endpoint: config.storageEndpoint,
+  forcePathStyle: config.storageForcePathStyle,
+});
+
+export async function exists(bucket, key) {
+  const input = {
+    Bucket: bucket,
+    Key: key,
+  };
+  const command = new HeadObjectCommand(input);
+  let result = true;
+  try { await client.send(command); }
+  catch (err) {
+    if (err.message === "UnknownError") {
+      result = false;
+    }
+    else {
+      throw err;
+    }
+  }
+  return result;
+}
+
+export async function generateUrl(bucket, key) {
+  const url = new URL(
+    `${bucket}/${key}`,
+    config.minioEndpoint,
+  );
+  return url.href;
+}
+
+export async function store(bucket, key, data, compress = false) {
+  const passThroughStream = new stream.PassThrough();
+  const uploadsRequest = new Upload({
+    client,
+    params: {
+      Bucket: bucket,
+      Key: key,
+      Body: passThroughStream,
+      ACL: "public-read",
+    },
+  });
+
+  if (compress) {
+    pipeline(
+      data,
+      zlib.createGzip(),
+      passThroughStream
+    );
+  }
+  else {
+    pipeline(
+      data,
+      passThroughStream
+    );
+  }
+
+  return uploadsRequest.done();
+}
+
+export async function retrieve(bucket, key, decompress = false) {
+  const command = new GetObjectCommand({
+    Bucket: bucket,
+    Key: key,
+  });
+  const response = await client.send(command);
+
+  if (decompress) {
+    return response.Body.pipe(zlib.createGunzip());
+  }
+  else {
+    return response.Body;
+  }
+}
