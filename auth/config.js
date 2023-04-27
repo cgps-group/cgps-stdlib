c699c4f9340d9e134933bc2705910333a9d229bd
Khalil <khalil.abudahab@cgps.group>
2023-04-27 22:37:18 +0100

    Update config.js

--- auth/config.js
+++ auth/config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  google: {
-    clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
-    clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
-  },
+  google: (
+    (process.env.AUTH_GOOGLE_CLIENT_ID && process.env.AUTH_GOOGLE_CLIENT_SECRET)
+      ?
+      {
+        clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
+        clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
+      }
+      :
+      undefined
+  ),
   secret: process.env.AUTH_SECRET,
 };
