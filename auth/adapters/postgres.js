5d70d33948367da967c0b15ae79fa82d4e3aec1c
Khalil <khalil.abudahab@cgps.group>
2023-05-19 10:37:59 +0100

    Delete postgres.js

--- auth/adapters/postgres.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable new-cap */
-
-const { MongoDBAdapter } = require("@next-auth/mongodb-adapter");
-
-const clientPromise = require("../../db/mongodb");
-
-module.exports = MongoDBAdapter(clientPromise);
