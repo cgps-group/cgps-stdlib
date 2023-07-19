883e1723b55347e7d03a71db3a68f80cf271cef9
Khalil <khalil.abudahab@cgps.group>
2023-07-19 23:32:11 +0100

    Update require-user.js

--- middleware/require-user.js
+++ middleware/require-user.js
@@ -2,8 +2,8 @@ import { ApiError } from "next/dist/server/api-utils";
 
 import getUserMiddleware from "./get-user.js";
 
-async function requireUserMiddleware(req, res) {
-  const user = await getUserMiddleware(req, res);
+async function requireUserMiddleware(req, res, adapter) {
+  const user = await getUserMiddleware(req, res, adapter);
 
   if (!user) {
     throw new ApiError(401, "Unauthorized");
