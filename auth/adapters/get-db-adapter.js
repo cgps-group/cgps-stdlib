bc4dac3a5b25076cbe7ca7775961dd8ad77f250f
Khalil <khalil.abudahab@cgps.group>
2023-07-19 04:47:38 +0100

    Update get-db-adapter.js

--- auth/adapters/get-db-adapter.js
+++ auth/adapters/get-db-adapter.js
@@ -1,7 +1,7 @@
 import serverRuntimeConfig from "../../config/server-runtime-config.js";
 
-if (typeof serverRuntimeConfig.auth.adapter !== "function") {
+if (typeof serverRuntimeConfig.auth.adapter !== "object") {
   throw new Error("auth requires an adapter");
 }
 
-export default serverRuntimeConfig.auth.adapter();
+export default serverRuntimeConfig.auth.adapter;
