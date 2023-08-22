5d601c883423dad79cf7d553fc1f6354d485e774
Khalil <khalil.abudahab@cgps.group>
2023-08-22 15:12:55 +0100

    Update markdown.js

--- ui/markdown.js
+++ ui/markdown.js
@@ -3,15 +3,41 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
-const components = { p: React.Fragment };
+function LinkRenderer(props) {
+  return (
+    <a
+      href={props.href}
+      rel="noreferrer"
+      target="_blank"
+    >
+      {props.children}
+    </a>
+  );
+}
+
+LinkRenderer.propTypes = {
+  children: PropTypes.node,
+  href: PropTypes.string,
+};
+
+const defaultComponents = {
+  a: LinkRenderer,
+};
+
+const unwrapComponents = {
+  ...defaultComponents,
+  p: React.Fragment,
+};
+
 const plugins = [remarkGfm];
 
 const Markdown = (
   (props) => {
     return (
       <ReactMarkdown
-        remarkPlugins={props.unwrap ? plugins : undefined}
-        components={props.unwrap ? components : undefined}
+        // remarkPlugins={props.unwrap ? plugins : undefined}
+        remarkPlugins={plugins}
+        components={props.unwrap ? unwrapComponents : defaultComponents}
       >
         { props.children }
       </ReactMarkdown>
