017551b64eceaea590ef9dddcca7da07700ac734
Khalil <khalil.abudahab@cgps.group>
2023-09-28 13:07:31 +0100

    Update pino.js

--- logger/pino.js
+++ logger/pino.js
@@ -1,5 +1,13 @@
 import pino from "pino";
 
+let transport;
+if (process.env.NODE_ENV === "development") {
+  transport = {
+    target: "pino-pretty",
+    options: { colorize: true },
+  };
+}
+
 const logger = pino({
   bindings: (bindings) => {
     return {
@@ -15,17 +23,7 @@ const logger = pino({
   },
   level: process.env.LOGGER_LEVEL ?? "silent",
   timestamp: pino.stdTimeFunctions.isoTime,
-  ...(process.env.NODE_ENV === "development"
-    ? {
-      transport: {
-        target: "pino-pretty",
-        options: {
-          colorize: true,
-        },
-      },
-    }
-    : {}
-  ),
+  transport,
 });
 
 export default logger;
