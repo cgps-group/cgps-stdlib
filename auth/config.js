32d47f1c0c475cd2530478572dcf9860ca3621ca
Khalil <khalil.abudahab@cgps.group>
2023-11-16 20:40:12 +0000

    Update config.js

--- auth/config.js
+++ auth/config.js
@@ -1,23 +1,36 @@
-module.exports = {
-  google: (
-    (process.env.AUTH_GOOGLE)
-      ?
-      {
-        clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
-        clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
-      }
-      :
-      undefined
-  ),
-  bryn: (
-    (process.env.AUTH_BRYN)
-      ?
-      {
-        clientId: process.env.AUTH_BRYN_CLIENT_ID,
-        clientSecret: process.env.AUTH_BRYN_CLIENT_SECRET,
-      }
-      :
-      undefined
-  ),
-  secret: process.env.AUTH_SECRET,
+const options = {
+  "secret": process.env.AUTH_SECRET,
 };
+
+if (process.env.AUTH_GOOGLE) {
+  options.google = {
+    clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
+    clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
+  };
+}
+
+if (process.env.AUTH_AZURE_AD) {
+  options["azure-ad"] = {
+    clientId: process.env.AUTH_AZURE_AD_CLIENT_ID,
+    clientSecret: process.env.AUTH_AZURE_AD_CLIENT_SECRET,
+    tenantId: process.env.AUTH_AZURE_AD_TENANT_ID,
+  };
+}
+
+if (process.env.AUTH_AZURE_AD_B2C) {
+  options["azure-ad-b2c"] = {
+    tenantId: process.env.AUTH_AZURE_AD_B2C_TENANT_NAME,
+    clientId: process.env.AUTH_AZURE_AD_B2C_CLIENT_ID,
+    clientSecret: process.env.AUTH_AZURE_AD_B2C_CLIENT_SECRET,
+    primaryUserFlow: process.env.AUTH_AZURE_AD_B2C_PRIMARY_USER_FLOW,
+  };
+}
+
+if (process.env.AUTH_BRYN) {
+  options.bryn = {
+    clientId: process.env.AUTH_BRYN_CLIENT_ID,
+    clientSecret: process.env.AUTH_BRYN_CLIENT_SECRET,
+  };
+}
+
+module.exports = options;
