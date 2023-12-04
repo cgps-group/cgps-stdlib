74f155f34e3a65b3b1e3c8130167ef6bf6d4e772
Khalil <khalil.abudahab@cgps.group>
2023-12-04 15:27:14 +0000

    Update index.js

--- ui/data-grid/index.js
+++ ui/data-grid/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import naturalCompare from "natural-compare";
 
 import { EmptyObject } from "../../constants.js";
 
-function DataGrid(props) {
+import styles from "./index.module.css";
+
+function GridSection(props) {
   return (
     <Grid
       container
@@ -31,6 +35,67 @@ function DataGrid(props) {
         )
       }
     </Grid>
+  );
+}
+
+GridSection.propTypes = {
+  Card: PropTypes.func.isRequired,
+  cardProps: PropTypes.object.isRequired,
+  items: PropTypes.array.isRequired,
+};
+
+function DataGrid(props) {
+  const groupedItems = React.useMemo(
+    () => {
+      if (props.groupBy) {
+        const groups = {};
+        for (const item of props.items) {
+          const group = props.groupBy(item);
+          if (group in groups) {
+            groups[group].push(item);
+          }
+          else {
+            groups[group] = [ item ];
+          }
+        }
+        return Object.entries(groups).sort((a, b) => naturalCompare(a[0], b[0]));
+      }
+      else {
+        return undefined;
+      }
+    },
+    [props.groupBy, props.items],
+  );
+
+  if (groupedItems) {
+    return (
+      (groupedItems).map(
+        ([ group, items ]) => {
+          return (
+            <React.Fragment key={group}>
+              <Typography
+                className={styles.header}
+              >
+                {group}
+              </Typography>
+              <GridSection
+                Card={props.Card}
+                cardProps={props.cardProps}
+                items={items}
+              />
+            </React.Fragment>
+          );
+        }
+      )
+    );
+  }
+
+  return (
+    <GridSection
+      Card={props.Card}
+      cardProps={props.cardProps}
+      items={props.items}
+    />
   );
 }
 
