2a6758830272fb47461586035cc9b11a70ff8767
Khalil <khalil.abudahab@cgps.group>
2024-02-06 14:42:05 +0000

    Update df.js

--- themes/df.js
+++ themes/df.js
@@ -37,7 +37,7 @@ const theme = {
   },
   fonts: {
     headline: `"Montserrat Variable", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif`,
-    body: `"Work Sans Variable", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif`,
+    body: `"Open Sans Variable", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif`,
   },
 };
 
