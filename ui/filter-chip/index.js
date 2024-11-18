6c5f55ce3641485ecee637ed4a3ef92539d00731
Khalil <khalil.abudahab@cgps.group>
2024-11-18 15:54:50 +0000

    Add FilterChip component.

--- ui/filter-chip/index.js
+++ ui/filter-chip/index.js
@@ -2,24 +2,37 @@ import React from "react";
 import PropTypes from "prop-types";
 import Chip from "@mui/material/Chip";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ClearIcon from "@mui/icons-material/Clear";
 
 import styles from "./index.module.css";
 
-function FilterChip(props) {
+function DeleteIcon(props) {
   return (
-    <Chip
-      className={styles.root}
-      label="Clickable Deletable"
-      variant="outlined"
+    <button
       onClick={props.onClick}
-      onDelete={props.onClick}
-      deleteIcon={<ArrowDropDownIcon />}
+      className={styles["delete-button"]}
     >
-      {props.children}
-      <ArrowDropDownIcon />
-    </Chip>
+      <ClearIcon />
+    </button>
   );
 }
+
+const FilterChip = React.forwardRef((props, ref) => {
+  return (
+    <Chip
+      ref={ref}
+      className={styles.root}
+      label={props.children}
+      variant="filled"
+      onClick={props.onClick}
+      onDelete={props.onDelete}
+      deleteIcon={<DeleteIcon />}
+    >
+    </Chip>
+  );
+});
+
+FilterChip.displayName = "FilterChip";
 
 FilterChip.propTypes = {
   children: PropTypes.node.isRequired,
