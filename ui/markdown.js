49aa88206eeb8f2ffdc471c6d39b40212b3870bc
Khalil Abu-Dahab <khalil.abudahab@cgps.group>
2026-04-29 21:26:50 +0100

    Update markdown.js

--- ui/markdown.js
+++ ui/markdown.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import Link from "@mui/material/Link";
 
 function LinkRenderer(props) {
   return (
@@ -24,26 +25,57 @@ const defaultComponents = {
   a: LinkRenderer,
 };
 
+const markdownComponents = {
+  a({ href, children, ...props }) {
+    return (
+      <Link
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        underline="always"
+        {...props}
+      >
+        {children}
+      </Link>
+    );
+  },
+};
+
 const unwrapComponents = {
   ...defaultComponents,
   p: React.Fragment,
 };
 
-const plugins = [remarkGfm];
+const plugins = [
+  remarkGfm,
+];
 
-const Markdown = (
-  (props) => {
-    return (
-      <ReactMarkdown
-        // remarkPlugins={props.unwrap ? plugins : undefined}
-        remarkPlugins={plugins}
-        components={props.unwrap ? unwrapComponents : defaultComponents}
-      >
-        { props.children }
-      </ReactMarkdown>
-    );
+function Markdown(
+  props
+) {
+  const markdownProps = {};
+  if (props.title) {
+    markdownProps.allowedElements=[ "em", "strong", "code" ];
+    markdownProps.unwrapDisallowed = true;
+    markdownProps.skipHtml = true;
+    markdownProps.components = unwrapComponents;
+    markdownProps.remarkPlugins = [];
   }
-);
+  if (props.description) {
+    markdownProps.components = markdownComponents;
+    markdownProps.remarkPlugins = [];
+  }
+  return (
+    <ReactMarkdown
+      // remarkPlugins={props.unwrap ? plugins : undefined}
+      remarkPlugins={plugins}
+      components={props.unwrap ? unwrapComponents : defaultComponents}
+      {...markdownProps}
+    >
+      { props.children }
+    </ReactMarkdown>
+  );
+}
 
 Markdown.displayName = "Markdown";
 
